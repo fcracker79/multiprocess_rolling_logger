760eeba4d2d3a3da4cc5fef11f34002a2e895080
mirko <fcracker79@gmail.com>
2016-02-10 19:45:10 +0100

    Fixed race condition of fcntl in Linux.

--- mplogger/rolling.py
+++ mplogger/rolling.py
@@ -1,21 +1,33 @@
 from logging import handlers, LogRecord
 import fcntl
+import os
+import sys
+
 
 def mprolling(supertype, *args, **kwargs):
     class _MP(supertype):
         def __init__(self, *args, **kwargs):
             supertype.__init__(self, *args, **kwargs)
+            self.fd = open(self.baseFilename + '__lock__', 'a')
 
         def doRollover(self):
-            with open(self.baseFilename, 'a') as fd:
-                try:
-                    fcntl.lockf(fd, fcntl.LOCK_EX)
-                    print('DENTRO')
-                    if self.shouldRollover(LogRecord(None, None, None, None, None, None, None)):
-                        supertype.doRollover(self)
-                finally:
-                    fcntl.lockf(fd, fcntl.LOCK_UN)
-                    print('FUORI')
+            try:
+                fcntl.flock(self.fd, fcntl.LOCK_EX)
+                print('DENTRO{}'.format(os.getpid()))
+                sys.stdout.flush()
+                if self.shouldRollover(LogRecord(None, None, None, None, None, None, None)):
+                    print('\tROLLOVER{}'.format(os.getpid()))
+                    sys.stdout.flush()
+                    print('\tROLLOVER END{}'.format(os.getpid()))
+                    supertype.doRollover(self)
+            finally:
+                fcntl.flock(self.fd, fcntl.LOCK_UN)
+                print('FUORI{}'.format(os.getpid()))
+                sys.stdout.flush()
+
+        def close(self):
+            self.fd.close()
+
     return _MP(*args, **kwargs)
 
 def MPRotatingFileHandler(*args, **kwargs):
